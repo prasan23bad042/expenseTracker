e4061532e2c251003c830e8ef122a4bdb3e674c1
sai prasan <kit27.ad42@gmail.com>
2024-10-12 09:00:00 +0530

    Add getAllCategories method to ExpenseDAO

--- src/main/java/com/Expense-Tracker/dao/expenseDAO.java
+++ src/main/java/com/Expense-Tracker/dao/expenseDAO.java
@@ -1,7 +1,27 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import com.Expense-Tracker.model.Category;
+import com.Expense-Tracker.model.Expense;
+import com.Expense-Tracker.util.DatabaseConnection;
+import java.time.LocalDateTime;
+
 public class ExpenseDAO{
     private static final String SELECT_ALL_EXPENSES = "SELECT * FROM expenses";
-
-
-
-    public Expense getExpense()
+    private static final String SELECT_ALL_CATEGORIES = "SELECT * FROM categories";
+    
+    public List<Category> getAllCategories() throws SQLException {
+        List<Category> categories = new ArrayList<>();
+        try (Connection conn = DatabaseConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(SELECT_ALL_CATEGORIES)) {
+            while (rs.next()) {
+                Category category = new Category();
+                category.setId(rs.getInt("id"));
+                category.setName(rs.getString("name"));
+                categories.add(category);
+            }
+        }
+        return categories;
+    }
 }
